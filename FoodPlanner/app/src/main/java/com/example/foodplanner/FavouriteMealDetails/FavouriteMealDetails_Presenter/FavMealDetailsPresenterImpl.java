16a8bb0b1c5a02ba46d42c851c07661d95711586
SarahTalaat <sarahtalaatammar.work@gmail.com>
2024-02-17 23:24:34 +0200

    trying with favourite1

--- FoodPlanner/app/src/main/java/com/example/foodplanner/FavouriteMealDetails/FavouriteMealDetails_Presenter/FavMealDetailsPresenterImpl.java
+++ FoodPlanner/app/src/main/java/com/example/foodplanner/FavouriteMealDetails/FavouriteMealDetails_Presenter/FavMealDetailsPresenterImpl.java
@@ -2,9 +2,12 @@ package com.example.foodplanner.FavouriteMealDetails.FavouriteMealDetails_Presen
 
 //import androidx.lifecycle.LiveData;
 
+import android.util.Log;
+
 import com.example.foodplanner.FavouriteMealDetails.FavouriteMealDetails_View.FavMealDetailsView;
-import com.example.foodplanner.FavouriteMealDetails.FavouriteMealDetails_View.FavouriteMealDetailsActivity;
+import com.example.foodplanner.FavouriteMealDetails.FavouriteMealDetails_View.FavouriteMealDetails_Fragment;
 import com.example.foodplanner.MealDetails.MealDetails_Model.MealDetails;
+import com.example.foodplanner.MealDetails.MealDetails_Model.MealDetailsRepository;
 import com.example.foodplanner.MealDetails.MealDetails_Model.MealDetailsRepositoryImplementation;
 import com.example.foodplanner.R;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
@@ -12,17 +15,22 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class FavMealDetailsPresenterImpl implements FavMealDetailsPresenter {
 
-    MealDetailsRepositoryImplementation repository;
+    MealDetailsRepository repository;
     FavMealDetailsView view;
 
-    public FavMealDetailsPresenterImpl(){
-
-    }
-
-    public FavMealDetailsPresenterImpl(FavouriteMealDetailsActivity favMealDetailsActivity, MealDetailsRepositoryImplementation mealDetailssRepository) {
+/*
+    public FavMealDetailsPresenterImpl(FavouriteMealDetails_Fragment favouriteMealDetails_fragment, MealDetailsRepositoryImplementation mealDetailssRepository) {
         this.repository = mealDetailssRepository;
-        this.view = favMealDetailsActivity;
+        this.view = favouriteMealDetails_fragment;
     }
+
+ */
+    public FavMealDetailsPresenterImpl(FavMealDetailsView favMealDetailsView, MealDetailsRepository mealDetailsRepository){
+        this.view = favMealDetailsView;
+        this.repository=mealDetailsRepository;
+    }
+
+
 /*
     @Override
     public void getStoredMealDetails() {
@@ -41,6 +49,11 @@ public class FavMealDetailsPresenterImpl implements FavMealDetailsPresenter {
 
     @Override
     public void removeFromFav(MealDetails mealDetails) {
-        repository.deleteMealDetails(mealDetails);
-}
+        repository.deleteMealDetails(mealDetails).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(() -> view.removeMealDetails(mealDetails),
+                error -> Log.i("RemoveFromFavourite", "removeFromFav: ERRORRRRR ")
+        );
+
+    }
 }
