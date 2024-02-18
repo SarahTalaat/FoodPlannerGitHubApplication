548ba4a1415b0bfe8145e252108abd05b73d8fab
SarahTalaat <sarahtalaatammar.work@gmail.com>
2024-02-18 21:19:22 +0200

    trying to display the meals in the planner

---
+++ FoodPlanner/app/src/main/java/com/example/foodplanner/Plan/Plan_View/PlanViewInterface.java
@@ -0,0 +1,11 @@
+package com.example.foodplanner.Plan.Plan_View;
+
+import com.example.foodplanner.Plan.Plan_Model.Plan;
+
+public interface PlanViewInterface {
+
+    void deleltemealplan(Plan plan);
+    void showdatamealplan();
+    void showerrormealplan(String errormsg);
+
+}
